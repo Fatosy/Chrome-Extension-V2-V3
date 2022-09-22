432c63d3292b4e2453e1de8d7d1d76d755688444
fatosy <fatosy_code@163.com>
2022-09-22 10:35:54 +0800

    'addAPIcheckmd'

--- myStudy/v2/api/js/content.js
+++ myStudy/v2/api/js/content.js
@@ -1,11 +1,9 @@
-console.log("----back.js start------")
+console.log("----content.js start------")
 console.log(chrome)
-console.log(chrome.scripting)
 for(let key1 in chrome){
     for(let key2 in chrome[key1]){
         let names = 'chrome.' + key1 + '.' + key2
         console.log(names)
     }
 }
-console.log("----back.js end------")
-
+console.log("----content.js end------")
---
+++ myStudy/v2/api/js/tab.js
@@ -0,0 +1,5 @@
+function getCurrentTabId(callback){
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        if(callback) callback(tabs.length ? tabs[0].id : null);
+    });
+}
